91455bb0b4f5b47b045587a4e7ad7291f94145a7
xaae4k5 <kolessnikovbiznes@gmail.com>
2025-04-13 12:07:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -72,26 +72,62 @@ player2 = Player2('obz2.png', 600,215,68, 100, 10)
 ball = Ball('ball.png', 350-25, 250-25, 50,50,5)
 ball.direct[0] = choice([-1,1])
 ball.direct[1] = choice([-1,1])
+
+btn = GameSprite('play.png', (700-137)/2, (500-72)/2, 137, 72, 0)
+
+font.init()
+font_1 = font.SysFont('Arial', 36)
+win = ''
+
 game = True
 score_r = 0
 score_l = 0
 rule = 3
 clock = time.Clock()
 FPS = 40
+menu = True
+finish = True
 
 while game:
     window.blit(background,(0,0))
     for e in event.get():
         if e.type == QUIT:
             game = False
-    player1.update()
-    player1.reset()
-
-    player2.update()
-    player2.reset()
-    
-    ball.update()
-    ball.reset()
+    if menu:
+        window.blit(background, (0,0))
+        btn.reset()
+        pressed = mouse.get_pressed()
+        pos = mouse.get_pos()
+        if pressed [0]:
+            if btn.rect.collidepoint(pos[0], pos[1]):
+                menu = False
+                finish = False
+                win = ''
+                score_l = 0
+                score_r = 0
+        if win != '':
+            winner = font_1.render('Победил ' +win+'!', 1, (255,255,255))
+            window.blit(winner, (150,350))
+            scr = font_1.render('со счетом' +str(max(score_l, score_r)) +':'+str(min(score_l, score_r)), 1, (255,255,255))
+            window.blit(scr, (250,400))
+    if not finish:
+        window.blit(background, (0,0))
+        player1.update()
+        player1.reset()
+        player2.update()
+        player2.reset()
+        ball.update()
+        ball.reset()
+        scr_left = font_1.render(str(score_l), 1, (255, 255, 255))
+        window.blit(scr_left, (10,10))
+        if score_l>=3:
+            win = 'левый игрок'
+            menu = True
+            finish = True
+        if score_r>=3:
+            win = 'правый игрок'
+            menu = True
+            finish = True
 
     clock.tick(FPS)
     display.update()
